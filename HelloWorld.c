0fbb9f17c1c1cc101b916af90d5394c00c635026
celliott12 <35587078+celliott12@users.noreply.github.com>
2018-01-25 14:52:54 -0600

    Wrote hello world

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+/**
+  Author: Conner Elliott
+  Date: 1/25/2018
+  Hello World
+*/ 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv) {
+	printf("Conner Elliott");
+	return 0;
+}
